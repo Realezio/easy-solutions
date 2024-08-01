56ca97ff86d54f3e91ddc944927df3da86711b35
Faisal HUSSAIN <faisal.hussain@outlook.fr>
2024-08-01 23:24:59 +0200

    chore: Add daisyui plugin for bumblebee theme and update npm dependencies

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,24 @@
 <template>
-  <main>
-    <slot />
-  </main>
+  <div data-theme="bumblebee">
+    <main>
+      <slot />
+    </main>
+    <footer class="footer bg-neutral text-neutral-content p-10">
+      <aside>
+        <img
+          src="/logo/logo-white.svg"
+          alt=""
+        >
+        <p>
+          EASY SOLUTIONS SASU
+          <br>
+          SIREN 982 791 006
+        </p>
+      </aside>
+    </footer>
+  </div>
 </template>
+
+<script lang="ts" setup>
+
+</script>
